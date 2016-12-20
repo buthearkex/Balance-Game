16c5e6cdb8c4c142ad6a9b5e42dbfa1f657bc983
Butterkex <mikko.s.honkanen@aalto.fi>
2016-12-20 13:08:29 +0100

    Initial commit

---
+++ balancer/GameScene.swift
@@ -0,0 +1,86 @@
+//
+//  GameScene.swift
+//  balancer
+//
+//  Created by Mikko on 13/12/2016.
+//  Copyright (c) 2016 Mikko. All rights reserved.
+//
+
+import CoreMotion
+import SpriteKit
+
+class GameScene: SKScene {
+    
+    
+    var motionManager: CMMotionManager!
+    var player: SKSpriteNode!
+    
+    
+    override func didMoveToView(view: SKView) {
+        /* Setup your scene here */
+        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
+        myLabel.text = "Balancer"
+        myLabel.fontSize = 45
+        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
+        
+        
+        self.addChild(myLabel)
+        createPlayer()
+        
+        //removing the gravity from the player
+        physicsWorld.gravity = CGVector(dx: 0, dy: 0)
+        
+        //start the sensors
+        motionManager = CMMotionManager()
+        motionManager.startAccelerometerUpdates()
+        
+        // 1
+        let borderBody = SKPhysicsBody(edgeLoopFromRect: self.frame)
+        // 2
+        borderBody.friction = 0
+        // 3
+        self.physicsBody = borderBody
+        
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+       /* Called when a touch begins */
+        
+        /*for touch in touches {
+            let location = touch.locationInNode(self)
+            
+            let sprite = SKSpriteNode(imageNamed:"Spaceship")
+             
+            sprite.xScale = 0.5
+            sprite.yScale = 0.5
+            sprite.position = location
+            
+            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
+            
+            sprite.runAction(SKAction.repeatActionForever(action))
+            
+            self.addChild(sprite)
+        }*/
+    }
+   
+    override func update(currentTime: CFTimeInterval) {
+        /* Called before each frame is rendered */
+        if let accelerometerData = motionManager.accelerometerData {
+            physicsWorld.gravity = CGVector(dx: accelerometerData.acceleration.y * -50, dy: accelerometerData.acceleration.x * 50)
+        }
+    }
+    
+    func createPlayer() {
+        player = SKSpriteNode(imageNamed: "SKSpriteNode")
+        player.position = CGPoint(x: 500, y: 672)
+        player.physicsBody = SKPhysicsBody(circleOfRadius: player.size.width / 2)
+        player.physicsBody!.allowsRotation = false
+        player.physicsBody!.linearDamping = 0.5
+        
+        /*player.physicsBody!.categoryBitMask = CollisionTypes.player.rawValue
+        player.physicsBody!.contactTestBitMask = CollisionTypes.star.rawValue | CollisionTypes.vortex.rawValue | CollisionTypes.finish.rawValue
+        player.physicsBody!.collisionBitMask = CollisionTypes.wall.rawValue*/
+        addChild(player)
+    }
+    
+}
