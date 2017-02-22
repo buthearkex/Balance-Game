b15882ba0a1f776dbf59e73d16f483cd2aee1f82
Butterkex <mikko.s.honkanen@aalto.fi>
2017-02-22 10:47:17 +0100

    framework2

---
+++ SpriteKitSimpleGame/TremorTrackerFramework.framework/Headers/TremorTrackerFramework.h
@@ -0,0 +1,19 @@
+//
+//  TremorTrackerFramework.h
+//  TremorTrackerFramework
+//
+//  Created by Thomas Fett on 21.02.17.
+//  Copyright © 2017 thomasfett. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TremorTrackerFramework.
+FOUNDATION_EXPORT double TremorTrackerFrameworkVersionNumber;
+
+//! Project version string for TremorTrackerFramework.
+FOUNDATION_EXPORT const unsigned char TremorTrackerFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TremorTrackerFramework/PublicHeader.h>
+
+
